5d127cd9caf55ccab9cef8d9980757b38ddd88ad
homedm <weasel.wt@gmail.com>
2019-06-19 02:26:02 +0900

    Update: add command options

--- mono_con.py
+++ mono_con.py
@@ -1,37 +1,43 @@
+# coding: utf-8
+
+""" ものコン用スクリプト
+
+Usage:
+    $ python3 mono_con.py --graph
+
+"""
+
 ### import processing.serial.*
 ### import cc.arduino.*
 
-
-
-### python
-import math
-import numpy as np
+import argparse
 import pygame
-
-from pygame.locals import *
+### from pygame.locals import *
+import numpy as np
 from matplotlib    import pyplot as plt
+### from pyfirmata2    import Arduino, util
 ### from psonic        import *
-from pyfirmata2    import Arduino, util
 
 ### my created
 from graph   import Graph
 from vector  import Vector
 from reader  import Reader
 
-
 def setup():
+    global args
+    args = get_args()
+
     plt.ion()           # 対話モードオン
+
     global frame
     frame = 0
     ###  ------------------------------------------------
     global v, v1
-    v  = Vector(0, 0, 0)
+    v = Vector(0, 0, 0)
     v1 = Vector(0, 0, 0)
     ### --------------------------------------------------
     global reader
-    reader = Reader('/dev/ttyACM0')
-    ### ### ### reader = Reader('COM4')
-
+    reader = Reader(args.port)
     reader.start(0)
     reader.start(1)
     reader.start(2)
@@ -47,8 +53,8 @@ def setup():
     graph_r.bias = 0.5
     ### --------------------------------------------------
     global sound
-    pygame.mixer.init(frequency = 22050, size = 8, channels = 1, buffer = 1024)
-    sound = pygame.mixer.Sound("sound/machdash1.wav")
+    pygame.mixer.init(frequency=22050, size=8, channels=1, buffer=1024)
+    sound = pygame.mixer.Sound(args.file)
 
 def draw():
     global frame
@@ -59,36 +65,52 @@ def draw():
     v.x = reader.analog[4].value
     ### v.normalize()
     ### render graph -------------------------------------
-    graph_a.update( frame, v.distance() )
-    graph_l.update( frame, reader.analog[0].value )
-    graph_r.update( frame, reader.analog[1].value )
+    graph_a.update(frame, v.distance())
+    graph_l.update(frame, reader.analog[0].value)
+    graph_r.update(frame, reader.analog[1].value)
 
+    if args.graph:
+        graph_a.render(1, "dis")
+        graph_l.render(3, "l")
+        graph_r.render(5, "r")
 
-    #graph_a.render(1, "dis")
-    #graph_l.render(3, "l")
-    #graph_r.render(5, "r")
+        plt.draw()
+        plt.pause(0.01) ### 05)
+        plt.clf()
 
-    #plt.draw()
-    #plt.pause(0.01) ### 05)
-    #plt.clf()
-
-    b_a = np.std(graph_a.val_y[-3:])            > 0.01
+    b_a = np.std(graph_a.val_y[-3:]) > 0.01
     b_l = graph_l.val_y[-2] - graph_l.val_y[-1] > 0.05
     b_r = graph_r.val_y[-2] - graph_r.val_y[-1] > 0.05
     b_result = b_a and (b_l or b_r)
-    if frame <  50: return 0
-    if frame == 50: print('start')
 
-    print('a: {}\t l: {}\t r: {}\t all: {}'.format(b_a, b_l, b_r, b_result))
+    if frame < 50:
+        return 0
 
-    if  b_result :
-        print('success')
+    if args.log:
+        print('a: {}\t l: {}\t r: {}\t all: {}'.format(b_a, b_l, b_r, b_result))
+
+    if  b_result:
+        if args.log:
+            print('success')
         sound.play()
-        ### play (60, amp=1, attack=0, decay=1, sustain_level=0,sustain=0, release=0)
-    else:
-        pass
-        ### sound.stop()
-        ### play (60, amp=0, attack=0, decay=1, sustain_level=0,sustain=0, release=0)
+
+def get_args():
+    parser = argparse.ArgumentParser(description='This script is developed for mono con at 2019.')
+    parser.add_argument('--graph',
+                        action='store_true',
+                        help='show graph')
+    parser.add_argument('--log',
+                        action='store_true',
+                        help='show log')
+    parser.add_argument('--port',
+                        type=str, default='/dev/ttyACM0',
+                        help='set connected port')
+    parser.add_argument('--file',
+                        type=str,
+                        default='sound/machdash1.wav',
+                        help='specify sound file path')
+
+    return parser.parse_args()
 
 if __name__ == '__main__':
     setup()
